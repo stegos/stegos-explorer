0ab19c4ad6fa6319c9198f5d4f958f84b1b23100
Joel Reymont <joelr1@gmail.com>
2020-05-18 19:54:02 +0300

    Drastically speed up the macroblock list query

--- explorer_backend/migrations/2020-05-18_downsize_and_index/down.sql
+++ explorer_backend/migrations/2020-05-18_downsize_and_index/down.sql
@@ -1,3 +1,10 @@
 DROP INDEX micro_blocks_by_epoch;
 DROP INDEX macro_blocks_by_epoch;
+DROP FUNCTION num_transactions;
+
+CREATE FUNCTION num_transactions(BIGINT, TEXT) RETURNS BIGINT
+    AS 'select count(*) from micro_blocks inner join transactions on micro_blocks.block_hash = any(micro_block_hash) where epoch=$1 and network=$2;'
+    LANGUAGE SQL
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;
 
